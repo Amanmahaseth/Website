22ecc5289980d0e3a8e90183f062a70a699affa3
suraj <surajido111@gmail.com>
2020-02-29 17:24:03 +0545

    update

--- eudomonia/urls.py
+++ eudomonia/urls.py
@@ -6,15 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('communicable/', views.communicable),
-=======
-<<<<<<< HEAD
     path('accounts/', include('accounts.urls')),
-    
-=======
-    path('communicable/', views.communicable),
->>>>>>> 5517c7ec4ce7ccd291c84a9a2330fae103817b83
->>>>>>> 486ef563ca6953d51c6b7725462db99ad7be4287
-    
 ]
